52f6a2b15e1f291ac3783b5a71d1c9e2b5099ed2
Hong Li <hong_li@cable.comcast.com>
2020-11-05 08:10:47 -0500

    Fix compile errors with plat build

--- server/plat/rtabstractservice.cpp
+++ server/plat/rtabstractservice.cpp
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-#include "rtabstractservice.h"
+#include "rtabstractservice.hpp"
 rtAbstractService::rtAbstractService(rtString serviceName)
     : mServiceName(serviceName)
     , mApiVersion(1), mEmit()
