c5c1c68b7318d08e68a2a355ca76911b14f76cb0
Akhil Baby Sarada <akhil_sarada@comcast.com>
2023-08-21 16:31:39 +0530

    RDKTV-24917,RDKTV-22355,RDKTV-24153 : gdial Crash issue
    Reason for change: Added exception to Webserver default IP address
    Test Procedure: Refer JIRA
    Risks: Medium
    Priority : p1
    Signed-off-by: Akhil Baby Sarada Akhil_Sarada@comcast.com

--- server/plat/scripts/restartXdial_onAddressChange.sh
+++ server/plat/scripts/restartXdial_onAddressChange.sh
@@ -98,7 +98,7 @@ if [ "$1" == "add" ] && [ "$2" == "ipv4" ] && [ "$3" == "$XDIAL_IFNAME" ];then
     fi
     echo "file count for ls /tmp/XDIAL_* : $file_count \n " >> $LOG_FILE
     if [ $file_count -gt 0 ]; then
-      if [ $4 != "192.168.28.10" ] && [ $4 != "192.168.18.10" ] && [ $4 != "192.0.2.11" ] && [ $4 != "192.0.2.10" ]; then
+      if [ $4 != "192.168.28.10" ] && [ $4 != "192.168.18.10" ] && [ $4 != "192.0.2.11" ] && [ $4 != "192.0.2.10" ] && [$4 != "10.0.0.3"]; then
         printf "IP change to new valid ip: $4 proceed with restart  " >> $LOG_FILE
         restartXdialService &
       else
