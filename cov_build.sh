6750ebaf408e15a46cad36f53fae7966e8f869aa
yuvaramachandran_gurusamy <yuvaramachandran_gurusamy@comcast.com>
2025-06-05 19:20:11 +0000

    RDKEMW-4129: Test
    Signed-off-by: yuvaramachandran_gurusamy <yuvaramachandran_gurusamy@comcast.com>

--- cov_build.sh
+++ cov_build.sh
@@ -9,15 +9,8 @@ ls -la ${GITHUB_WORKSPACE}
 echo "buliding xdialserver"
 
 cd ${GITHUB_WORKSPACE}
-cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/xdialserver \
--DUSE_THUNDER_R4=ON \
--DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
--DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
--DCMAKE_VERBOSE_MAKEFILE=ON \
--DCMAKE_CXX_FLAGS="-DEXCEPTIONS_ENABLE=ON \
--I ${GITHUB_WORKSPACE}/Thunder/Source \
--I ${GITHUB_WORKSPACE}/Thunder/Source/core" \
 
-cmake --build build/xdialserver --target install
+make
+
 echo "======================================================================================"
 exit 0
