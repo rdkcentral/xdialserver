7302ede2a9fafc17ca36b2c25999fe2156d43f89
ssitar583 <soundaryaa_sitaram@comcast.com>
2025-06-26 11:19:31 +0530

    Update gdial.cpp

--- server/plat/gdial.cpp
+++ server/plat/gdial.cpp
@@ -82,11 +82,6 @@ public:
                 reterror = GDIAL_CAST_ERROR_NONE;
             }
         }
-        if (nullptr != AppObj)
-        {
-            delete AppObj;
-            AppObj = nullptr;
-        }
         GDIAL_LOGTRACE("Exiting ...");
         return reterror;
     }
