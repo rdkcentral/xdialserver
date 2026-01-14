6460dda38a60ab2bfe88fb7166fb252c7d741440
agampa263 <133222558+agampa263@users.noreply.github.com>
2026-01-14 12:34:18 +0530

    Update build_dependencies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -10,7 +10,7 @@ cd ${GITHUB_WORKSPACE}
 #1. Install Dependencies and packages
 
 sudo apt update
-sudo apt install -y ninja-build meson curl libsoup2.4-dev libxml2-dev libglib2.0-dev gobject-introspection libgirepository1.0-dev libgtk-3-dev libcunit1-dev valac pandoc
+sudo apt install -y ninja-build meson curl libsoup2.4-dev libxml2-dev libglib2.0-dev gobject-introspection libgirepository1.0-dev libgtk-3-dev valac pandoc
 pip install jsonref
 
 ############################
@@ -70,9 +70,7 @@ cmake -G Ninja -S ThunderTools -B build/ThunderTools \
     -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
     -DGENERIC_CMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
 
-DESTDIR="$GITHUB_WORKSPACE/install" \
 cmake --build build/ThunderTools --target install
-
 
 
 ############################
