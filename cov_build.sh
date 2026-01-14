eaed86fd67fecb7bf5cf5262176dbf924e6e905f
agampa263 <133222558+agampa263@users.noreply.github.com>
2026-01-14 15:19:25 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -10,6 +10,8 @@ export PKG_CONFIG_PATH="$INSTALL_DIR/lib/pkgconfig:$INSTALL_DIR/lib64/pkgconfig:
 export LD_LIBRARY_PATH="$INSTALL_DIR/lib:$INSTALL_DIR/lib64:$INSTALL_DIR/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
 export PATH="$INSTALL_DIR/bin:$INSTALL_DIR/sbin:$PATH"
 
+export CMAKE_PREFIX_PATH="$INSTALL_DIR:$CMAKE_PREFIX_PATH"
+
 ls -la "${GITHUB_WORKSPACE}"
 
 ############################
