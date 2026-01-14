03e596e7b1a2055dbc0a7e8b294a22e946f4e688
agampa263 <133222558+agampa263@users.noreply.github.com>
2026-01-14 14:27:47 +0530

    Update build_dependencies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -1,130 +1,146 @@
 #!/bin/bash
-set -x
 set -e
+set -x
+
 ##############################
+# Workspace setup
 GITHUB_WORKSPACE="${PWD}"
+INSTALL_DIR="$GITHUB_WORKSPACE/install"
 
-cd ${GITHUB_WORKSPACE}
+mkdir -p "$INSTALL_DIR"
 
-# # ############################# 
-#1. Install Dependencies and packages
+export PATH="$INSTALL_DIR/bin:$INSTALL_DIR/sbin:$PATH"
+export LD_LIBRARY_PATH="$INSTALL_DIR/lib:$INSTALL_DIR/lib64:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="$INSTALL_DIR/lib/pkgconfig:$INSTALL_DIR/lib64/pkgconfig:$PKG_CONFIG_PATH"
 
+cd "$GITHUB_WORKSPACE"
+
+##############################
+# 1. Install system dependencies (still needs sudo)
 sudo apt update
-sudo apt install -y ninja-build meson curl libsoup2.4-dev libxml2-dev libglib2.0-dev gobject-introspection libgirepository1.0-dev libgtk-3-dev libcunit1-dev valac pandoc
+sudo apt install -y \
+    ninja-build meson curl \
+    libsoup2.4-dev libxml2-dev libglib2.0-dev \
+    gobject-introspection libgirepository1.0-dev \
+    libgtk-3-dev libcunit1-dev \
+    valac pandoc
+
 pip install jsonref
 
-############################
-# Build trevor-base64
+##############################
+# Build trower-base64
 if [ ! -d "trower-base64" ]; then
-git clone https://github.com/xmidt-org/trower-base64.git
+    git clone https://github.com/xmidt-org/trower-base64.git
 fi
-cd trower-base64
-meson setup --warnlevel 3 --werror build
-ninja -C build
-sudo ninja -C build install
-cd ..
-###########################################
-# Clone the required repositories
 
+cd trower-base64
+meson setup build --prefix="$INSTALL_DIR"
+ninja -C build
+ninja -C build install
+cd ..
+
+##############################
+# Clone repositories
 rm -rf iarmbus ThunderTools Thunder entservices-apis entservices-testframework gssdp
 
-
 git clone https://github.com/rdkcentral/iarmbus.git
-export IARMBUS_PATH=$GITHUB_WORKSPACE/iarmbus
+export IARMBUS_PATH="$GITHUB_WORKSPACE/iarmbus"
 
-git clone --branch  R4.4.3 https://github.com/rdkcentral/ThunderTools.git
-
+git clone --branch R4.4.3 https://github.com/rdkcentral/ThunderTools.git
 git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
-
 git clone --branch main https://github.com/rdkcentral/entservices-apis.git
-
 git clone https://$GITHUB_TOKEN@github.com/rdkcentral/entservices-testframework.git
-
 git clone --branch gssdp-1.2.3 https://gitlab.gnome.org/GNOME/gssdp.git
 
-############################
-# Build gssdp-1.2
+##############################
+# Build gssdp
 echo "======================================================================================"
-echo "buliding gssdp-1.2"
+echo "building gssdp"
+
 cd gssdp
-
-rm -rf build
-meson setup build
-
+meson setup build --prefix="$INSTALL_DIR"
 ninja -C build
-sudo ninja -C build install
-cd -
+ninja -C build install
+cd ..
 
-############################
-# Build Thunder-Tools
+##############################
+# Build ThunderTools
 echo "======================================================================================"
-echo "buliding thunderTools"
-cd ThunderTools
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/00010-R4.4-Add-support-for-project-dir.patch
-cd -
+echo "building ThunderTools"
 
+cd ThunderTools
+patch -p1 < "$GITHUB_WORKSPACE/entservices-testframework/patches/00010-R4.4-Add-support-for-project-dir.patch"
+cd ..
 
 cmake -G Ninja -S ThunderTools -B build/ThunderTools \
-    -DEXCEPTIONS_ENABLE=ON \
-    -DCMAKE_INSTALL_PREFIX="/usr" \
-    -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
-    -DGENERIC_CMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+    -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+    -DCMAKE_MODULE_PATH="$INSTALL_DIR/tools/cmake" \
+    -DGENERIC_CMAKE_MODULE_PATH="$INSTALL_DIR/tools/cmake"
 
 cmake --build build/ThunderTools --target install
 
-
-############################
+##############################
 # Build Thunder
 echo "======================================================================================"
-echo "buliding thunder"
+echo "building Thunder"
 
 cd Thunder
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/Use_Legact_Alt_Based_On_ThunderTools_R4.4.3.patch
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/error_code_R4_4.patch
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/1004-Add-support-for-project-dir.patch
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/RDKEMW-733-Add-ENTOS-IDS.patch
-cd -
+patch -p1 < "$GITHUB_WORKSPACE/entservices-testframework/patches/Use_Legact_Alt_Based_On_ThunderTools_R4.4.3.patch"
+patch -p1 < "$GITHUB_WORKSPACE/entservices-testframework/patches/error_code_R4_4.patch"
+patch -p1 < "$GITHUB_WORKSPACE/entservices-testframework/patches/1004-Add-support-for-project-dir.patch"
+patch -p1 < "$GITHUB_WORKSPACE/entservices-testframework/patches/RDKEMW-733-Add-ENTOS-IDS.patch"
+cd ..
 
 cmake -G Ninja -S Thunder -B build/Thunder \
     -DMESSAGING=ON \
-    -DCMAKE_INSTALL_PREFIX="/usr" \
-    -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
-    -DGENERIC_CMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+    -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+    -DCMAKE_MODULE_PATH="$INSTALL_DIR/tools/cmake" \
+    -DGENERIC_CMAKE_MODULE_PATH="$INSTALL_DIR/tools/cmake" \
     -DBUILD_TYPE=Debug \
     -DBINDING=127.0.0.1 \
     -DPORT=55555 \
-    -DEXCEPTIONS_ENABLE=ON \
+    -DEXCEPTIONS_ENABLE=ON
 
 cmake --build build/Thunder --target install
 
-############################
+##############################
 # Build entservices-apis
 echo "======================================================================================"
-echo "buliding entservices-apis"
+echo "building entservices-apis"
+
 cd entservices-apis
 rm -rf jsonrpc/DTV.json
 cd ..
 
-cmake -G Ninja -S entservices-apis  -B build/entservices-apis \
-    -DEXCEPTIONS_ENABLE=ON \
-    -DCMAKE_INSTALL_PREFIX="/usr" \
-    -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+cmake -G Ninja -S entservices-apis -B build/entservices-apis \
+    -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+    -DCMAKE_MODULE_PATH="$INSTALL_DIR/tools/cmake" \
+    -DEXCEPTIONS_ENABLE=ON
 
 cmake --build build/entservices-apis --target install
 
-############################
-
-############################
-# Build and deploy stubs for IARMBus and WPEFramework securityagent
-mkdir -p /usr/include/WPEFramework/securityagent
-cp $GITHUB_WORKSPACE/stubs/securityagent/* /usr/include/WPEFramework/securityagent/ -v
-
+##############################
+# Build IARMBus & SecurityAgent stubs (local)
 echo "======================================================================================"
-echo "Building IARMBus and WPEFramework securityagent stubs"
-cd $GITHUB_WORKSPACE
-cd ./stubs
-g++ -fPIC -shared -o libIARMBus.so iarm_stubs.cpp -I$GITHUB_WORKSPACE/stubs -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I$IARMBUS_PATH/core -I$IARMBUS_PATH/core/include -fpermissive
-g++ -fPIC -shared -o libWPEFrameworkSecurityUtil.so securityagent/SecurityTokenUtil.cpp  -I$GITHUB_WORKSPACE/stubs -fpermissive
+echo "Building stubs"
 
-cp libIARMBus.so /usr/local/lib/
-cp libWPEFrameworkSecurityUtil.so /usr/local/lib/
+mkdir -p "$INSTALL_DIR/include/WPEFramework/securityagent"
+cp stubs/securityagent/* "$INSTALL_DIR/include/WPEFramework/securityagent/"
+
+cd stubs
+
+g++ -fPIC -shared -o libIARMBus.so iarm_stubs.cpp \
+    -I"$GITHUB_WORKSPACE/stubs" \
+    -I"$INSTALL_DIR/include" \
+    -I"$IARMBUS_PATH/core" \
+    -I"$IARMBUS_PATH/core/include" \
+    -fpermissive
+
+g++ -fPIC -shared -o libWPEFrameworkSecurityUtil.so \
+    securityagent/SecurityTokenUtil.cpp \
+    -I"$GITHUB_WORKSPACE/stubs" \
+    -I"$INSTALL_DIR/include" \
+    -fpermissive
+
+cp libIARMBus.so "$INSTALL_DIR/lib/"
+cp libWPEFrameworkSecurityUtil.so "$INSTALL_DIR/lib/"
