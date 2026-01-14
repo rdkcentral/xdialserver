6abd04cb3d55a197fdca586f9f28e71986709399
agampa263 <133222558+agampa263@users.noreply.github.com>
2026-01-14 17:11:27 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -5,19 +5,17 @@ set -x
 GITHUB_WORKSPACE="${PWD}"
 INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr"
 
-##############################
-# Environment
 export CMAKE_PREFIX_PATH="$INSTALL_PREFIX"
 export PKG_CONFIG_PATH="$INSTALL_PREFIX/lib/pkgconfig:$INSTALL_PREFIX/lib/x86_64-linux-gnu/pkgconfig"
 export LD_LIBRARY_PATH="$INSTALL_PREFIX/lib:$INSTALL_PREFIX/lib/x86_64-linux-gnu"
 
-##############################
-# Build xdialserver (out-of-source)
 cmake -G Ninja \
   -S "$GITHUB_WORKSPACE/server" \
   -B build/xdialserver \
   -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
   -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
-  -DCMAKE_CXX_FLAGS="-I$INSTALL_PREFIX/include/WPEFramework/core"
+  -DCMAKE_CXX_FLAGS="\
+-I$INSTALL_PREFIX/include/WPEFramework/core \
+-I$INSTALL_PREFIX/include/WPEFramework/plugins"
 
 cmake --build build/xdialserver
