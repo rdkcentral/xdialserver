e51ecb3ccd0829f027e6843be8647757eb575cc7
agampa263 <AkshayKumar_Gampa@comcast.com>
2026-01-15 18:57:23 +0530

    RDKEMW-12555 : Fix coveirty workflow scan in xdialserver repo
    Reason for Change: Fix coveirty scan workflow failure in xdialserver repo
    Test Procedure: Verify coveirty workflow
    Risks: Low
    Priority: P1
    version: minor
    Signed-off-by:AkshayKumar_Gampa AkshayKumar_Gampa@comcast.com

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -10,7 +10,7 @@ cd ${GITHUB_WORKSPACE}
 #1. Install Dependencies and packages
 
 apt update
-apt install -y ninja-build meson curl libsoup2.4-dev libxml2-dev libglib2.0-dev gobject-introspection libgirepository1.0-dev libgtk-3-dev valac pandoc
+apt install -y git python3 python3-pip cmake ninja-build meson curl libsoup2.4-dev libxml2-dev libglib2.0-dev gobject-introspection libgirepository1.0-dev libgtk-3-dev libcurl4-openssl-dev libgtk-3-dev libcunit1-dev valac pandoc
 pip install jsonref
 
 ############################
