02d51125deea45612ff73a4b905c158688f4e491
agampa263 <AkshayKumar_Gampa@comcast.com>
2026-01-15 18:30:39 +0530

    test

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -10,7 +10,10 @@ cd ${GITHUB_WORKSPACE}
 #1. Install Dependencies and packages
 
 apt update
-apt install -y ninja-build meson curl libsoup2.4-dev libxml2-dev libglib2.0-dev gobject-introspection libgirepository1.0-dev libgtk-3-dev valac pandoc
+apt install -y ninja-build meson cmake curl libcurl4-openssl-dev \
+  libsoup2.4-dev libxml2-dev libglib2.0-dev \
+  gobject-introspection libgirepository1.0-dev \
+  libgtk-3-dev libcunit1-dev valac pandoc
 pip install jsonref
 
 ############################
