620185a99513abdb4540b98122c3b7e6b1247580
Simon Chung <simon.chung@rdkcentral.com>
2026-01-15 10:34:32 +0000

    Switch native_full_build container to ubuntu:22.04

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -10,6 +10,10 @@ cd ${GITHUB_WORKSPACE}
 #1. Install Dependencies and packages
 
 apt update
+apt install -y python3
+apt install -y python3-pip
+apt install -y cmake
+apt install -y libcunit1 libcunit1-doc libcunit1-dev
 apt install -y ninja-build meson curl libsoup2.4-dev libxml2-dev libglib2.0-dev gobject-introspection libgirepository1.0-dev libgtk-3-dev valac pandoc
 pip install jsonref
 
