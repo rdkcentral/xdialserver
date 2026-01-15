830567ca1c6ff9dde3cda1b35a894c6f5f9d2e6a
Simon Chung <simon.chung@rdkcentral.com>
2026-01-15 10:46:26 +0000

    Add apt install libcurl4-openssl-dev

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -15,6 +15,7 @@ apt install -y python3
 apt install -y python3-pip
 apt install -y cmake
 apt install -y libcunit1 libcunit1-doc libcunit1-dev
+apt install -y libcurl4-openssl-dev
 apt install -y ninja-build meson curl libsoup2.4-dev libxml2-dev libglib2.0-dev gobject-introspection libgirepository1.0-dev libgtk-3-dev valac pandoc
 pip install jsonref
 
