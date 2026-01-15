fdf65e215a17c67c4a3de4ceeb13e66c4ca1051a
agampa263 <133222558+agampa263@users.noreply.github.com>
2026-01-15 15:04:48 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-set -x
 set -e
+set -x
+
 ##############################
+# Paths
 GITHUB_WORKSPACE="${PWD}"
-ls -la ${GITHUB_WORKSPACE}
-############################
-# Build xdialserver
-echo "buliding xdialserver"
+INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr"
 
-cd ${GITHUB_WORKSPACE}
+##############################
+# Environment
+export CMAKE_PREFIX_PATH="$INSTALL_PREFIX"
+export PKG_CONFIG_PATH="$INSTALL_PREFIX/lib/pkgconfig:$INSTALL_PREFIX/lib/x86_64-linux-gnu/pkgconfig"
+export LD_LIBRARY_PATH="$INSTALL_PREFIX/lib:$INSTALL_PREFIX/lib/x86_64-linux-gnu"
 
-make
+##############################
+# Build xdialserver (out-of-source)
+cmake -G Ninja \
+  -S "$GITHUB_WORKSPACE/server" \
+  -B build/xdialserver \
+  -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
+  -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+  -DCMAKE_CXX_FLAGS="\
+-I$INSTALL_PREFIX/include/WPEFramework \
+-I$INSTALL_PREFIX/include/WPEFramework/core \
+-I$INSTALL_PREFIX/include/WPEFramework/plugins \
+-I$INSTALL_PREFIX/include/WPEFramework/interfaces" \
+  -DCMAKE_SHARED_LINKER_FLAGS="-L$INSTALL_PREFIX/lib" \
+  -DCMAKE_EXE_LINKER_FLAGS="-L$INSTALL_PREFIX/lib" \
+  -DCMAKE_BUILD_RPATH="$INSTALL_PREFIX/lib" \
+  -DCMAKE_INSTALL_RPATH="$INSTALL_PREFIX/lib"
 
-echo "======================================================================================"
-exit 0
+cmake --build build/xdialserver
