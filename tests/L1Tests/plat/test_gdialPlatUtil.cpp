300275e7a4bf52275060b690276ea203705a5675
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2026-04-15 10:27:25 -0400

    RDKEMW-16911 - L1 unit tests for xdialserver

--- tests/L1Tests/plat/test_gdialPlatUtil.cpp
+++ tests/L1Tests/plat/test_gdialPlatUtil.cpp
@@ -18,7 +18,9 @@
  */
 
 #include <gtest/gtest.h>
+#include <cctype>
 #include <cstring>
+#include <cstdlib>
 
 extern "C" {
 #include "gdial-plat-util.h"
@@ -36,6 +38,18 @@ TEST(GDialPlatUtilTest, GetIfaceIpv4Addr_LoopbackLooksValid) {
     EXPECT_STREQ(ip, "127.0.0.1");
 }
 
+TEST(GDialPlatUtilTest, GetIfaceIpv4Addr_UsesStableStaticBuffer) {
+    const char *ip1 = gdial_plat_util_get_iface_ipv4_addr("lo");
+    ASSERT_NE(ip1, nullptr);
+
+    const char *ip2 = gdial_plat_util_get_iface_ipv4_addr("lo");
+    ASSERT_NE(ip2, nullptr);
+
+    /* Implementation returns a pointer to a static internal buffer. */
+    EXPECT_EQ(ip1, ip2);
+    EXPECT_STREQ(ip2, "127.0.0.1");
+}
+
 TEST(GDialPlatUtilTest, GetIfaceMacAddr_InvalidIfaceReturnsNull) {
     const char *mac = gdial_plat_util_get_iface_mac_addr("iface_does_not_exist_123");
     EXPECT_EQ(mac, nullptr);
@@ -48,13 +62,38 @@ TEST(GDialPlatUtilTest, GetIfaceMacAddr_LoopbackLooksLikeMac) {
     for (size_t i = 0; i < 17; ++i) {
         if ((i + 1) % 3 == 0) {
             EXPECT_EQ(mac[i], ':');
+        } else {
+            EXPECT_TRUE(std::isxdigit(static_cast<unsigned char>(mac[i])) != 0);
         }
     }
+}
+
+TEST(GDialPlatUtilTest, GetIfaceMacAddr_UsesStableStaticBuffer) {
+    const char *mac1 = gdial_plat_util_get_iface_mac_addr("lo");
+    ASSERT_NE(mac1, nullptr);
+
+    const char *mac2 = gdial_plat_util_get_iface_mac_addr("lo");
+    ASSERT_NE(mac2, nullptr);
+
+    /* Implementation returns a pointer to a static internal buffer. */
+    EXPECT_EQ(mac1, mac2);
 }
 
 TEST(GDialPlatUtilLogTest, LoggerInitAndSetLevelDoNotCrash) {
     gdial_plat_util_logger_init();
     gdial_plat_util_set_loglevel(INFO_LEVEL);
+    SUCCEED();
+}
+
+TEST(GDialPlatUtilLogTest, LoggerInit_UsesEnvLogLevelAndSyncStdoutDoNotCrash) {
+    setenv("SYNC_STDOUT", "1", 1);
+    setenv("GDIAL_LIBRARY_DEFAULT_LOG_LEVEL", "4", 1);
+
+    gdial_plat_util_logger_init();
+    gdial_plat_util_log(VERBOSE_LEVEL, "fn", "file.c", 3, 125, "verbose after env");
+
+    unsetenv("SYNC_STDOUT");
+    unsetenv("GDIAL_LIBRARY_DEFAULT_LOG_LEVEL");
     SUCCEED();
 }
 
@@ -66,5 +105,11 @@ TEST(GDialPlatUtilLogTest, LogAtFatalDoesNotCrash) {
 TEST(GDialPlatUtilLogTest, LogAtVerboseDoesNotCrash) {
     gdial_plat_util_set_loglevel(TRACE_LEVEL);
     gdial_plat_util_log(VERBOSE_LEVEL, "fn", "file.c", 2, 124, "verbose %s", "ok");
+    SUCCEED();
+}
+
+TEST(GDialPlatUtilLogTest, LogFilteredByLevelDoesNotCrash) {
+    gdial_plat_util_set_loglevel(INFO_LEVEL);
+    gdial_plat_util_log(TRACE_LEVEL, "fn", "file.c", 4, 126, "trace should be filtered");
     SUCCEED();
 }
