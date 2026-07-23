5a151dde33e14062cb371c29b35e422ad9e6d7aa
balaji velmurugan <balaji_velmurugan@comcast.com>
2026-07-23 15:58:10 +0530

    RDKEMW-22210: Fix coverity issues on xdial
    Reason for change: Fixing coverity reported issues.
    Test Procedure: Refer ticket.
    Risks: Low
    Priority: P1
    version: minor
    
    Signed-off-by: balaji velmurugan <balaji_velmurugan@comcast.com>

--- server/gdial-app.c
+++ server/gdial-app.c
@@ -529,8 +529,12 @@ gchar * gdial_app_state_response_new(GDialApp *app, const gchar *dial_ver, const
   xmlNodePtr noptions = xmlNewChild(nservice, NULL, BAD_CAST "options", BAD_CAST NULL); {
     xmlNewProp(noptions, BAD_CAST "allowStop", BAD_CAST "true");
   }
-  xmlNewChild(nservice, NULL, BAD_CAST "state", BAD_CAST gdial_app_state_to_string(state));
-  if (app->state != GDIAL_APP_STATE_STOPPED) {
+  // coverity fix : USE_AFTER_FREE - check for NULL before using state_str
+  const gchar *state_str = gdial_app_state_to_string(state);
+  if (state_str) {
+    xmlNewChild(nservice, NULL, BAD_CAST "state", BAD_CAST state_str);
+  }
+  if (state != GDIAL_APP_STATE_STOPPED) {
     xmlNodePtr nlink  = xmlNewChild(nservice, NULL, BAD_CAST "link", BAD_CAST NULL);
     xmlNewProp(nlink, BAD_CAST "rel", BAD_CAST "run");
     xmlNewProp(nlink, BAD_CAST "href", BAD_CAST "run");
