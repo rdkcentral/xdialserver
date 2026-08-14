3f936f85f2f3014344bf76bb30a433a2316fd1fe
balav08 <54432605+balav08@users.noreply.github.com>
2026-08-14 18:46:44 +0530

    RDKMEW-22210 : Update build_dependencies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -2,10 +2,8 @@
 set -x
 set -e
 ##############################
-THUNDER_TOOLS_COMMIT_SHA="d5dd83c7c19c49c7f25c558c126500bd2d64f7a4"
-THUNDER_COMMIT_SHA="2c0fcc5529e7da734be558ca6efa05d934dcce31"
 GITHUB_WORKSPACE="${PWD}"
-ls -la ${GITHUB_WORKSPACE}
+
 cd ${GITHUB_WORKSPACE}
 
 # # ############################# 
@@ -34,19 +32,13 @@ rm -rf iarmbus ThunderTools Thunder entservices-apis entservices-testframework g
 git clone https://github.com/rdkcentral/iarmbus.git
 export IARMBUS_PATH=$GITHUB_WORKSPACE/iarmbus
 
-git clone --branch R4_4-RDK https://github.com/rdkcentral/ThunderTools.git
-cd ThunderTools
-git checkout $THUNDER_TOOLS_COMMIT_SHA
-cd ..
+git clone --branch  R4.4.3 https://github.com/rdkcentral/ThunderTools.git
 
-git clone --branch R4_4-RDK https://github.com/rdkcentral/Thunder.git
-cd Thunder
-git checkout $THUNDER_COMMIT_SHA
-cd ..
+git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
 
 git clone --branch main https://github.com/rdkcentral/entservices-apis.git
 
-git clone --branch 2.0.0 https://github.com/rdkcentral/entservices-testframework.git
+git clone https://$GITHUB_TOKEN@github.com/rdkcentral/entservices-testframework.git
 
 git clone --branch gssdp-1.2.3 https://gitlab.gnome.org/GNOME/gssdp.git
 
@@ -68,12 +60,13 @@ cd -
 echo "======================================================================================"
 echo "buliding thunderTools"
 cd ThunderTools
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/00010-R4.4-Add-support-for-project-dir.patch
 cd -
 
 
 cmake -G Ninja -S ThunderTools -B build/ThunderTools \
     -DEXCEPTIONS_ENABLE=ON \
-    -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
+    -DCMAKE_INSTALL_PREFIX="/usr" \
     -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
     -DGENERIC_CMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
 
@@ -86,11 +79,15 @@ echo "==========================================================================
 echo "buliding thunder"
 
 cd Thunder
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/Use_Legact_Alt_Based_On_ThunderTools_R4.4.3.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/error_code_R4_4.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/1004-Add-support-for-project-dir.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/RDKEMW-733-Add-ENTOS-IDS.patch
 cd -
 
 cmake -G Ninja -S Thunder -B build/Thunder \
     -DMESSAGING=ON \
-    -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
+    -DCMAKE_INSTALL_PREFIX="/usr" \
     -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
     -DGENERIC_CMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
     -DBUILD_TYPE=Debug \
@@ -110,7 +107,7 @@ cd ..
 
 cmake -G Ninja -S entservices-apis  -B build/entservices-apis \
     -DEXCEPTIONS_ENABLE=ON \
-    -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
+    -DCMAKE_INSTALL_PREFIX="/usr" \
     -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
 
 cmake --build build/entservices-apis --target install
